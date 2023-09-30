651e27816aa0c6bd43128396774685bf8243d015
alexgphan <alexander.jl.phan@gmail.com>
2023-09-29 20:37:56 -0400

    Start of Calendar class.

--- src/Calendar.java
+++ src/Calendar.java
@@ -8,6 +8,11 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import java.util.TreeMap;
 
+/**
+ * Class that creates a calendar that would contain holidays and class times used
+ * by all the employees.
+ * @param <T>
+ */
 public class Calendar<T extends Comparable<T>> {
     // This is a dictionary with String key Date and Value as classes
     private TreeMap<String, TreeMap<String, String>> classTimes;
@@ -19,16 +24,18 @@ public class Calendar<T extends Comparable<T>> {
     private TreeMap<String, String> holidays;
 
     public Calendar(){
-        classes = new TreeMap<String, String>;
-        classTimes = new TreeMap<String, TreeMap<String, String>>;
-        holidays = new Treemap<String, String>
+        classes = new TreeMap<String, String>();
+        classTimes = new TreeMap<String, TreeMap<String, String>>();
+        holidays = new TreeMap<String, String>();
     }
 
+    /**
+     * Adds classes/labs to the classes dictionary using className
+     * as the key and time as the value.
+     */
     public void addClasses(String className, String time)
     {
-        /**
-         * Puts className as a key in class and time as the value
-         */
+
         this.classes.put(className, time);
     }
 
