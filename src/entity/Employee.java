95aeb435907ddea24e4b4b45c34202a4ad1ede26
alexgphan <alexander.jl.phan@gmail.com>
2023-11-06 20:01:42 -0500

    Finishing up the Employee file

--- src/entity/Employee.java
+++ src/entity/Employee.java
@@ -1,19 +1,56 @@
-package entity;
+import java.util.HashMap;
 
-import jdk.vm.ci.meta.Local;
+/**
+ *
+ */
+abstract class Employee {
+    private String userID;
+    private String name;
+    private String email;
+    private String password;
+    // private ? myCourses; TODO: need to figure out what data structure to use
+    private HashMap<String, Courses> myCourses = new HashMap<String, Courses>();
+    // private ? mySessions; TODO: need to figure out what data structure to use
+    private HashMap<String, Sessions> mySessions = new HashMap<String, Sessions>();
 
-import java.util.Calendar;
-import java.util.Collection;
-import java.time.LocalDateTime
+    private Calendar myCalendar;
 
-public interface Employee {
-    String getUserID();
-    String getName();
-    String getEmail();
-    String getPassword();
-    Collection getMySessions();
-    LocalDateTime getlastTimeGotPayed();
-    Calendar getCalendar();
-    Collection getCourse();
-    void makeCalendar();
+    public Employee(String userID, String name, String email, String password) {
+        // TODO: implement this method & change the parameters
+
+        this.userID = userID;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getUID() {
+        return this.userID;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    // abstract ? getCourses(); TODO: need to figure out what type of data structure to use
+    public HashMap<String, Course> getCourses() {
+        return this.myCourses;
+    }
+    // abstract ? getSessions(); TODO: need to figure out what type of data structure to use
+    public HashMap<String, Session> getSessions() {
+        return this.mySessions;
+    }
+    abstract boolean addSession(Session session);
+    abstract boolean removeSession(); // TODO: how to identify which session to remove?
+
+    abstract boolean addCourse(Course course);
+
+    abstract boolean removeCourse(); // TODO: how to identify which course to remove?
+
+    abstract Calendar makeCalendar();
+
 }
