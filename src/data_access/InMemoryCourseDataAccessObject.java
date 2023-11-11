3c95dacca7737e0e1aff8a7131cae28973798513
Simon <bosen.xu@mail.utoronto.ca>
2023-11-10 21:56:33 -0500

    Added InMemoryCourseDataAccessObject.java and edited InMemoryEmployeeDataAccessObject.java to reduce number of imports.

---
+++ src/data_access/InMemoryCourseDataAccessObject.java
@@ -0,0 +1,25 @@
+package data_access;
+
+import entity.Course;
+import java.util.HashMap;
+import java.util.Set;
+
+public class InMemoryCourseDataAccessObject {
+    private final HashMap<String, Course> course = new HashMap<String, Course>();
+
+    public void save(Course course) {
+        this.course.put(course.getCourseCode());
+    }
+
+    public boolean existsByID(String courseCode) {
+        return course.containsKey(courseCode);
+    }
+
+    public Course getByID(String courseCode) {
+        return course.get(courseCode);
+    }
+
+    public Set<String> getAllIDs() {
+        return course.keySet();
+    }
+}
