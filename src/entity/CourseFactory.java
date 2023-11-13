8f544b1afd8c4e2f433589a97a79cc2c94934556
alexgphan <alexander.jl.phan@gmail.com>
2023-11-13 17:11:41 -0500

    Course and Instructor Factories added.

--- src/entity/CourseFactory.java
+++ src/entity/CourseFactory.java
@@ -1,2 +1,24 @@
-package entity;public class CourseFactory {
+package entity;
+
+import java.util.HashMap;
+
+public class CourseFactory {
+    public Course create(String name, String courseCode, HashMap<String, Employee> staff,
+                             HashMap<String, Event> events, Employee admin) {
+        Course course = new Course(name, courseCode, admin);
+
+        for (HashMap.Entry<String, Event> entry : events.entrySet()) {
+
+            Event value = entry.getValue();
+            course.addEvent(value);
+        }
+
+        for (HashMap.Entry<String, Employee> employees : staff.entrySet()) {
+
+            Employee value = employees.getValue();
+            course.addStaff(value);
+        }
+
+        return course;
+    }
 }
--- src/entity/InstructorFactory.java
+++ src/entity/InstructorFactory.java
@@ -1,2 +1,30 @@
-package entity;public class InstructorFactory {
+package entity;
+
+import java.util.HashMap;
+
+public class InstructorFactory {
+    public Instructor create(String userID, String name, String email, String password,
+                                    HashMap<String, ClassSession> sessions, HashMap<String, Course> courses) {
+        Instructor instructor = new Instructor(userID, name, email, password);
+
+        for (HashMap.Entry<String, ClassSession> entry : sessions.entrySet()) {
+            // Maybe I don't need to include getting the key
+            String key = entry.getKey();
+
+            ClassSession value = entry.getValue();
+            instructor.addSession(value);
+        }
+
+        for (HashMap.Entry<String, Course> entry : courses.entrySet()) {
+            // Maybe I don't need to include getting the key
+            String key = entry.getKey();
+
+            Course value = entry.getValue();
+            instructor.addCourse(value);
+        }
+
+        instructor.makeCalendar();
+
+        return instructor;
+    }
 }
