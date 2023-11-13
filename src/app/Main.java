64c3c291cc886ae7d97245ea8da29ba0133eb1d4
yoohamj <yooha.jung@mail.utoronto.ca>
2023-11-12 21:34:20 -0500

    Added extra parts to incorporate DashboardView in Main

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,6 +3,7 @@ package app;
 import interface_adapter.LoginViewModel;
 import interface_adapter.ViewManagerModel;
 import view.LoginView;
+import view.DashboardView;
 import view.ViewManager;
 
 import javax.swing.*;
@@ -21,23 +22,26 @@ public class Main {
 
         CardLayout cardLayout = new CardLayout();
 
-        // The various View objects. Only one view is visible at a time.
+        // Panel that contains the views.
         JPanel views = new JPanel(cardLayout);
         application.add(views);
 
-        // This keeps track of and manages which view is currently showing.
+        // Manages which view is currently showing.
         ViewManagerModel viewManagerModel = new ViewManagerModel();
         new ViewManager(views, cardLayout, viewManagerModel);
 
-        // The data for the views, such as username and password, are in the ViewModels.
-        // This information will be changed by a presenter object that is reporting the
-        // results from the use case. The ViewModels are observable, and will
-        // be observed by the Views.
+        // ViewModels for the views.
         LoginViewModel loginViewModel = new LoginViewModel();
 
-        LoginView loginView = new LoginView(loginViewModel);
+        // First, instantiate DashboardView.
+        DashboardView dashboardView = new DashboardView();
+        views.add(dashboardView, dashboardView.viewName);
+
+        // Now instantiate LoginView with reference to DashboardView's name.
+        LoginView loginView = new LoginView(loginViewModel, viewManagerModel, dashboardView.viewName);
         views.add(loginView, loginView.viewName);
 
+        // Set the initial view.
         viewManagerModel.setActiveView(loginView.viewName);
         viewManagerModel.firePropertyChanged();
 
