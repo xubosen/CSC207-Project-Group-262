c52cbb2a56a06a3bf39f0a4419d0e3df7b88eb8e
yoohamj <yooha.jung@mail.utoronto.ca>
2023-11-27 14:32:21 -0500

    Updated LoginController and LoginInputData to ensure data flows correctly

--- src/interface_adapter/LoginController.java
+++ src/interface_adapter/LoginController.java
@@ -1,2 +1,19 @@
-package interface_adapter;public class LoginController {
+package interface_adapter;
+
+import use_case.LoginInputBoundary;
+import use_case.LoginInputData;
+
+public class LoginController {
+
+    final LoginInputBoundary userLoginUseCaseInteractor;
+
+    public LoginController(LoginInputBoundary userLoginUseCaseInteractor) {
+        this.userLoginUseCaseInteractor = userLoginUseCaseInteractor;
+    }
+
+    public void execute(String username, String password) {
+        LoginInputData loginInputData = new LoginInputData(username, password);
+
+        userLoginUseCaseInteractor.execute(loginInputData);
+    }
 }
