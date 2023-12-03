66e164c077be80280c83d2d4e85dc13949bf82ba
yoohamj <yooha.jung@mail.utoronto.ca>
2023-12-02 20:54:56 -0500

    added createleave outputdata and boundary

--- src/use_case/create_leave/CreateLeaveOutputBoundary.java
+++ src/use_case/create_leave/CreateLeaveOutputBoundary.java
@@ -1,2 +1,8 @@
-package use_case.create_leave;public interface CreateLeaveOutputBoundary {
+package use_case.create_leave;
+
+import use_case.create_leave.CreateLeaveOutputData;
+
+public interface CreateLeaveOutputBoundary {
+    void prepareView(CreateLeaveOutputData outputData);
+
 }
--- src/use_case/create_leave/CreateLeaveOutputData.java
+++ src/use_case/create_leave/CreateLeaveOutputData.java
@@ -1,2 +1,32 @@
-package use_case.create_leave;public class CreateLeaveOutputData {
+package use_case.create_leave;
+
+public class CreateLeaveOutputData {
+    private boolean isSuccessful;
+    private String message;
+
+    /**
+     * Initialize whether leave creation was successful and the following related message.
+     * @param isSuccessful Boolean on whether leave creation worked
+     * @param message Message for success or failure.
+     */
+    public CreateLeaveOutputData(boolean isSuccessful, String message) {
+        this.isSuccessful = isSuccessful;
+        this.message = message;
+    }
+
+    /**
+     * Returns whether the output was successful.
+     * @return Whether leave creation was successful
+     */
+    public boolean isSuccessful() {
+        return isSuccessful;
+    }
+
+    /**
+     * Returns a success message or the type of failure that occurred if it failed to create a leave.
+     * @return Error or success message
+     */
+    public String getMessage() {
+        return message;
+    }
 }
